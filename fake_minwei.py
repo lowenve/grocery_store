130e960bc0d4c479e44b8dc7f02ff5848168dc11
lowenve <loongtext@163.com>
2019-12-06 14:47:29 +0800

    民族上100K～

--- fake_minwei.py
+++ fake_minwei.py
@@ -1,9 +1,11 @@
 import requests
+import time
 
 url = 'http://mzjs.cnki.net/exam/extraction'
-fout = open('result.txt', 'w')
+# fout = open('result.txt', 'w')
 for i in range(100000):
     r=requests.get(url)
-    fout.write(str(i+1)+' ： OK with status_code: '+str(r.text)+'\n')
-    print(str(i+1)+' ： OK with status_code: '+str(r.text)+'\n')
-fout.close()
+    # fout.write(str(i+1)+' ： OK with status_code: '+str(r.text)+'\n')
+    print(str(i+1)+'\n')
+    time.sleep(0.1)
+# fout.close()

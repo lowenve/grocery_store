720feb828e90d4df9aee5bf96af8c62c55dcd492
lowenve <loongtext@163.com>
2019-12-18 12:36:08 +0800

    😐删除了一个小程序文件

--- receive-mails-master/get_last_mail.py
+++ receive-mails-master/get_last_mail.py
@@ -1,13 +1,24 @@
 from email.parser import Parser
 from email.header import decode_header
 from email.utils import parseaddr
-
+from datetime import datetime
+import time
+import psycopg2
 import poplib
 
-# 输入邮件地址, 口令和POP3服务器地址:
-email = input('Email: ')
-password = input('Password: ')
-pop3_server = 'pop.'+email.split('@')[-1]
+
+def insetpgsql(msg):
+    # 连接到一个给定的数据库
+    conn_pg = psycopg2.connect(database="cnki_009_govin_pro_ods", user="postgres", password="fj5722902", host="10.170.128.121", port="5432")
+    cur_pg = conn_pg.cursor()
+    sql_pg = "INSERT INTO pyitchat VALUES ('{0}');".format(msg)
+    print(sql_pg)
+    cur_pg.execute(sql_pg)
+    conn_pg.commit()
+    # 关闭游标
+    cur_pg.close()
+    # 关闭数据库连接
+    conn_pg.close()
 
 
 def guess_charset(msg):
@@ -28,60 +39,76 @@ def decode_str(s):
 
 
 def print_info(msg, indent=0):
-    if indent == 0:
-        for header in ['From', 'To', 'Subject']:
-            value = msg.get(header, '')
-            if value:
-                if header == 'Subject':
-                    value = decode_str(value)
-                else:
-                    hdr, addr = parseaddr(value)
-                    name = decode_str(hdr)
-                    value = u'%s <%s>' % (name, addr)
-            print('%s%s: %s' % ('  ' * indent, header, value))
-    if (msg.is_multipart()):
-        parts = msg.get_payload()
-        for n, part in enumerate(parts):
-            print('%spart %s' % ('  ' * indent, n))
-            print('%s--------------------' % ('  ' * indent))
-            print_info(part, indent + 1)
+    if msg.get('To') != "root@loongtext.com":
+        print("非法邮件～")
     else:
-        content_type = msg.get_content_type()
-        if content_type == 'text/plain' or content_type == 'text/html':
-            content = msg.get_payload(decode=True)
-            charset = guess_charset(msg)
-            if charset:
-                content = content.decode(charset)
-            print('%sText: %s' % ('  ' * indent, content + '...'))
+        if indent == 0:
+            for header in ['From', 'To', 'Subject']:
+                value = msg.get(header, '')
+                if value:
+                    if header == 'Subject':
+                        value = decode_str(value)
+                    else:
+                        hdr, addr = parseaddr(value)
+                        name = decode_str(hdr)
+                        value = u'%s <%s>' % (name, addr)
+                print('%s%s: %s' % ('  ' * indent, header, value))
+        if (msg.is_multipart()):
+            parts = msg.get_payload()
+            for n, part in enumerate(parts):
+                print('%spart %s' % ('  ' * indent, n))
+                print('%s--------------------' % ('  ' * indent))
+                print_info(part, indent + 1)
         else:
-            print('%sAttachment: %s' % ('  ' * indent, content_type))
+            content_type = msg.get_content_type()
+            if content_type == 'text/plain' or content_type == 'text/html':
+                content = msg.get_payload(decode=True)
+                charset = guess_charset(msg)
+                if charset:
+                    content = content.decode(charset)
+                print('%sText: %s' % ('  ' * indent, content + '...'))
+                print(content)
+                insetpgsql(msg=content)
+            else:
+                print('%sAttachment: %s' % ('  ' * indent, content_type))
 
 
-# 连接到POP3服务器:
-server = poplib.POP3(pop3_server)
-# 可以打开或关闭调试信息:
-server.set_debuglevel(1)
-# 可选:打印POP3服务器的欢迎文字:
-print(server.getwelcome().decode('utf-8'))
-# 身份认证:
-server.user(email)
-server.pass_(password)
-# stat()返回邮件数量和占用空间:
-print('Messages: %s. Size: %s' % server.stat())
-# list()返回所有邮件的编号:
-resp, mails, octets = server.list()
-# 可以查看返回的列表类似[b'1 82923', b'2 2184', ...]
-print(mails)
-# 获取最新一封邮件, 注意索引号从1开始:
-index = len(mails)
-resp, lines, octets = server.retr(index)
-# lines存储了邮件的原始文本的每一行,
-# 可以获得整个邮件的原始文本:
-msg_content = b'\r\n'.join(lines).decode('utf-8')
-# 稍后解析出邮件:
-msg = Parser().parsestr(msg_content)
-print_info(msg)
-# 可以根据邮件索引号直接从服务器删除邮件:
-# server.dele(index)
-# 关闭连接:
-server.quit()
+if __name__ == '__main__':
+    # 输入邮件地址, 口令和POP3服务器地址:
+    # email = input('Email: ')
+    # password = input('Password: ')
+    email = "lowenve@foxmail.com"
+    password = "vqpfezncneetbfff"
+    pop3_server = 'pop.'+email.split('@')[-1]
+    while True:
+        # 连接到POP3服务器:
+        server = poplib.POP3(pop3_server)
+        # 可以打开或关闭调试信息:
+        server.set_debuglevel(1)
+        # 可选:打印POP3服务器的欢迎文字:
+        print(server.getwelcome().decode('utf-8'))
+        # 身份认证:
+        server.user(email)
+        server.pass_(password)
+        # stat()返回邮件数量和占用空间:
+        print('Messages: %s. Size: %s' % server.stat())
+        # list()返回所有邮件的编号:
+        resp, mails, octets = server.list()
+        # 可以查看返回的列表类似[b'1 82923', b'2 2184', ...]
+        # print(mails)
+        # 获取最新一封邮件, 注意索引号从1开始:
+        index = len(mails)
+        resp, lines, octets = server.retr(index)
+        # lines存储了邮件的原始文本的每一行,
+        # 可以获得整个邮件的原始文本:
+        msg_content = b'\r\n'.join(lines).decode('utf-8')
+        # 稍后解析出邮件:
+        msg = Parser().parsestr(msg_content)
+        print_info(msg)
+        # 可以根据邮件索引号直接从服务器删除邮件:
+        # server.dele(index)
+        # 关闭连接:
+        server.quit()
+
+        print(datetime.now().strftime("%Y-%m-%d  %H:%M:%S"))
+        time.sleep(5)
